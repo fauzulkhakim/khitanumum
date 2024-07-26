9f3a003f76b61a0ab1fa4ef6e7c7c7f0ca7481b5
Fauzul Khakim <studiopi7uaim@gmail.com>
2024-07-26 18:06:02 +0700

    update: menambahkan otp pada pendaftaran-tambah.php dan field di table pendaftar

--- panitia/config/khitanumum.sql
+++ panitia/config/khitanumum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 26 Jul 2024 pada 09.43
+-- Waktu pembuatan: 26 Jul 2024 pada 13.03
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -7298,6 +7298,7 @@ CREATE TABLE `pendaftar` (
   `nama_depan` varchar(255) NOT NULL,
   `nama_belakang` varchar(255) NOT NULL,
   `nik` varchar(16) NOT NULL,
+  `otp` int(6) NOT NULL,
   `status_pendaftaran_id` int(1) NOT NULL,
   `mustahiq` int(1) NOT NULL,
   `relasi` varchar(255) DEFAULT NULL,
@@ -7333,10 +7334,10 @@ CREATE TABLE `pendaftar` (
 -- Dumping data untuk tabel `pendaftar`
 --
 
-INSERT INTO `pendaftar` (`id`, `is_admin`, `nama_depan`, `nama_belakang`, `nik`, `status_pendaftaran_id`, `mustahiq`, `relasi`, `orang_tua_wali`, `no_hp`, `tempat_lahir_regencies_id`, `tanggal_lahir`, `alamat_lengkap`, `domisili_provinces_id`, `domisili_regencies_id`, `domisili_districts_id`, `domisili_villages_id`, `rt_rt_rw_id`, `rw_rt_rw_id`, `domisili`, `berat_badan`, `tinggi_badan`, `ukuran_baju_id`, `nama_sekolah`, `kelas_id`, `alamat_sekolah`, `dokumen_kia_kk`, `dokumen_sekolah`, `dokumen_domisili`, `dokumen_pendukung`, `name_created`, `date_created`, `name_updated`, `date_updated`) VALUES
-(1, 0, 'Fauzul', 'Khakim', '3319021904970001', 1, 0, NULL, 'Achmad', '085865068194', 3319, '2016-10-01', 'Jl Pasar Baru Gg Mangga III No 273', 33, 3319, 3319020, 3319020007, 5, 4, 1, '98', '158', 1, 'MA Qudsiyyah', 3, 'Kerjasan Kota Kudus', '469_3319021904970001.jpeg', '571_3319021904970001.jpeg', '', '', 'Umum', '2024-07-24 12:33:19', NULL, '2024-07-24 12:33:19'),
-(2, 0, 'Arik', 'Ashfa', '3319021904970002', 1, 0, NULL, 'Bachin', '085865068195', 3318, '2016-09-30', 'Kauman', 33, 3318, 3318050, 3318050009, 2, 3, 1, '60', '170', 3, 'Qudsiyyah', 9, 'Kota Kudus', '562_3319021904970002.jpeg', '70_3319021904970002.jpeg', '998_3319021904970002.jpg', '678_3319021904970002.jpeg', 'Umum', '2024-07-25 04:31:27', NULL, '2024-07-25 04:31:27'),
-(3, 0, 'Alham', 'Manazil', '3535353535353537', 1, 0, NULL, 'Achmad', '085865068194', 3319, '2008-11-06', 'pasuruhan lor kudus', 33, 3319, 3319030, 3319030006, 1, 3, 1, '65', '170', 3, 'sd2', 8, 'Kota Kudus', '796_3535353535353537.jpeg', '798_3535353535353537.jpeg', '504_3535353535353537.jpg', '18_3535353535353537.jpeg', 'Umum', '2024-07-26 07:23:10', NULL, '2024-07-26 07:23:10');
+INSERT INTO `pendaftar` (`id`, `is_admin`, `nama_depan`, `nama_belakang`, `nik`, `otp`, `status_pendaftaran_id`, `mustahiq`, `relasi`, `orang_tua_wali`, `no_hp`, `tempat_lahir_regencies_id`, `tanggal_lahir`, `alamat_lengkap`, `domisili_provinces_id`, `domisili_regencies_id`, `domisili_districts_id`, `domisili_villages_id`, `rt_rt_rw_id`, `rw_rt_rw_id`, `domisili`, `berat_badan`, `tinggi_badan`, `ukuran_baju_id`, `nama_sekolah`, `kelas_id`, `alamat_sekolah`, `dokumen_kia_kk`, `dokumen_sekolah`, `dokumen_domisili`, `dokumen_pendukung`, `name_created`, `date_created`, `name_updated`, `date_updated`) VALUES
+(1, 0, 'Fauzul', 'Khakim', '3319021904970001', 0, 1, 0, NULL, 'Achmad', '085865068194', 3319, '2016-10-01', 'Jl Pasar Baru Gg Mangga III No 273', 33, 3319, 3319020, 3319020007, 5, 4, 1, '98', '158', 1, 'MA Qudsiyyah', 3, 'Kerjasan Kota Kudus', '469_3319021904970001.jpeg', '571_3319021904970001.jpeg', '', '', 'Umum', '2024-07-24 12:33:19', NULL, '2024-07-24 12:33:19'),
+(2, 0, 'Arik', 'Ashfa', '3319021904970002', 0, 1, 0, NULL, 'Bachin', '085865068195', 3318, '2016-09-30', 'Kauman', 33, 3318, 3318050, 3318050009, 2, 3, 1, '60', '170', 3, 'Qudsiyyah', 9, 'Kota Kudus', '562_3319021904970002.jpeg', '70_3319021904970002.jpeg', '998_3319021904970002.jpg', '678_3319021904970002.jpeg', 'Umum', '2024-07-25 04:31:27', NULL, '2024-07-25 04:31:27'),
+(3, 0, 'Alham', 'Manazil', '3535353535353537', 0, 1, 0, NULL, 'Achmad', '085865068194', 3319, '2008-11-06', 'pasuruhan lor kudus', 33, 3319, 3319030, 3319030006, 1, 3, 1, '65', '170', 3, 'sd2', 8, 'Kota Kudus', '796_3535353535353537.jpeg', '798_3535353535353537.jpeg', '504_3535353535353537.jpg', '18_3535353535353537.jpeg', 'Umum', '2024-07-26 07:23:10', NULL, '2024-07-26 07:23:10');
 
 -- --------------------------------------------------------
 
